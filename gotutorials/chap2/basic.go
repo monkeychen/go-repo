dbe16a47a46b71de5b9c1e285360092a212a8c9a
monkeychen <cza55007@gmail.com>
2014-05-14 18:00:12 +0800

    add file

---
+++ gotutorials/chap2/basic.go
@@ -0,0 +1,8 @@
+package chap2
+
+import "fmt"
+
+var v1, v2, v3 int = 1, 2, 3
+func defineVar() {
+	fmt.Printf("v1 = %d, v2 = %d, v3 = %d", v1, v2, v3)
+}
---
+++ gotutorials/run.go
@@ -0,0 +1,12 @@
+/** #!go **/
+package main
+
+import (
+	"fmt"
+	"gotutorials/chap2"
+)
+
+func main() {
+	fmt.Printf("example-1:Hello, world")
+	chap2.defineVar()
+}
